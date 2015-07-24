8ec24ca1a673da12af2a0744fff75d3a8a9f436c
Enes Cakir <edencakir@gmail.com>
2015-07-24 23:20:14 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/iyte/irl/irl/Adapters/NewsActivityAdapter.java
@@ -0,0 +1,44 @@
+package tr.edu.iyte.irl.irl.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Enes on 7/24/15.
+ */
+public class NewsActivityAdapter extends RecyclerView.Adapter<NewsActivityAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<String> data;
+
+    //create constructor for adapter here.
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView test;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            //test = (TextView) itemView.findViewById(R.id. );
+        }
+    }
+}
---
+++ app/src/main/java/tr/edu/iyte/irl/irl/Model/NewsItem.java
@@ -0,0 +1,7 @@
+package tr.edu.iyte.irl.irl.Model;
+
+/**
+ * Created by Enes on 7/24/15.
+ */
+public class NewsItem {
+}
