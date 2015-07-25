795622706cbd85e4dede14040926defc29413789
Enes Cakir <edencakir@gmail.com>
2015-07-25 22:37:49 +0300

    Added toolbar, font file

--- app/src/main/java/tr/edu/iyte/irl/irl/NewsActivity.java
+++ app/src/main/java/tr/edu/iyte/irl/irl/NewsActivity.java
@@ -11,7 +11,7 @@ public class NewsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_item_news);
+        setContentView(R.layout.activity_news);
     }
 
     @Override
